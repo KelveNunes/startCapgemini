4f9529df6905d45e2fa4c4c4b6df804ccbb497f9
kelve <kelvenunes123@gmail.com>
2024-03-23 14:37:01 -0300

    add js-contato

---
+++ JAVASCRIPT/contato.js
@@ -0,0 +1,13 @@
+document.ready = function() {
+   
+    var nome = $("#nome").html();
+    var email = $("#email").html();
+    var tel = $("#tel").html();
+    var idade = $("#idade").html();
+    var linkedin = $("#linkedin").html();
+
+       
+   
+    alert("Nome: " + nome + "\nEmail: " + email + "\nTelefone: " + tel + "\nIdade: " + idade + "\nLinkedin: " + linkedin);
+
+}
